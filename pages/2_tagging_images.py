392b8a0cb84566588de4f558378005eaa3704e6d
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2025-02-05 10:12:05 +0530

    Update 2_tagging_images.py

--- pages/2_tagging_images.py
+++ pages/2_tagging_images.py
@@ -10,11 +10,15 @@ st.title("Tagging images")
 
 df = pd.read_csv(r"extracted_tags_images.csv")
 
+#initialize approved tags session state
 if "approved_tags" not in st.session_state:
     st.session_state.approved_tags = {}
 
+
+#save the csv in mongodb
 #save_dataframe_to_mongodb(dataframe = df, collection = IMG_COLLECTION)
 
+#load the csv from mongodb
 #input_df = load_dataframe_from_mongodb(collection = IMG_COLLECTION)
 input_df = df
 
@@ -34,9 +38,6 @@ def get_cached_file_ids():
 file_ids = get_cached_file_ids()
 
 
-
-
-
 images_folder = IMAGES_FOLDER
 
 #input_df = input_df.fillna("")
@@ -47,7 +48,12 @@ for index, row in input_df.iterrows():
     container = st.container(border = True)
     file_name = row["file"]
     text = row["text"]
-    tags = row["extracted_tags"].split(",")
+    extracted_tags = row["extracted_tags"].split(",")
+    approved_tags = row.get("approved_tags", "")
+
+    #if the session state doen't have approved tags then update it
+    if file_name not in st.session_state.approved_tags:
+        st.session_state.approved_tags[file_name] = approved_tags
 
     container = st.container(border = True)
     with container:
@@ -63,11 +69,9 @@ for index, row in input_df.iterrows():
                     file_ids[file_name] = file_id
 
             file_url = display_image_from_file_id(file_id)
-            #st.write(file_url)
+           # st.write(file_url)
             if file_url:
                 display_image(file_url)
-                #st.image(file_url, use_container_width=True, output_format="JPEG")
-                #display_image_from_file_id(file_id)
             else:
                 st.error("Image not found in Google Drive")
 
@@ -80,32 +84,41 @@ for index, row in input_df.iterrows():
                 previous_tags = ""
             previous_tags_list = previous_tags.split(", ") if previous_tags else []
 
-            selected_tags = st.multiselect(label = "Select tags",
-                                     options = tags,
-                                     default = previous_tags_list,
-                                     key = f"multi_select_{index}")
+            valid_defaults = [tag for tag in previous_tags_list if tag in extracted_tags]
 
-       
+            #wrap multi-select, manual text area & submit button in form structure
+            with st.form(key = f"form_{index}"):
+                selected_tags = st.multiselect(label = "Select tags",
+                                        options = extracted_tags,
+                                        default = valid_defaults,
+                                        key = f"multi_select_{index}")
 
-            new_tag = st.text_area("Enter new tags separated by commas", key = f"text_input_{index}", placeholder = "Enter tag")
+                new_tag = st.text_area("Enter new tags separated by commas", key = f"text_input_{index}", placeholder = "Enter tag")
+                submitted = st.form_submit_button("Save to database")
 
-            if new_tag:
-                new_tags = [tag.strip() for tag in new_tag.split(",") if tag.strip()]
-                selected_tags.extend(new_tags)
+            #after clicking on submit button
+            if submitted:
 
-            selected_tags = ", ".join(selected_tags)
-            st.session_state.approved_tags[file_name] = selected_tags
+                #if tags are entered in text area
+                if new_tag:
+                    new_tags = [tag.strip() for tag in new_tag.split(",") if tag.strip()]
+                    selected_tags.extend(new_tags)
 
-        if st.button("Save to MongoDB", key = f"save_{index}"):
-            updated_df = pd.DataFrame([{
-                "index": index,
-                "file": file_name,
-                "text": text,
-                "extracted_tags": row["extracted_tags"],
-                "approved_tags": st.session_state.approved_tags[file_name]
-            }])
-            st.write("will save to mongodb")
-            #update_dataframe_to_mongodb(dataframe = updated_df, collection = IMG_COLLECTION)         
+                #update the session state & save to MongoDB
+                selected_tags = ", ".join(selected_tags)
+                st.session_state.approved_tags[file_name] = selected_tags
+                updated_df = pd.DataFrame([{
+                    "index": index,
+                    "file": file_name,
+                    "text": text,
+                    "extracted_tags": row["extracted_tags"],
+                    "approved_tags": st.session_state.approved_tags[file_name]
+                }])
+                #update_dataframe_to_mongodb(dataframe = updated_df, collection = IMG_COLLECTION)
+
+            #display the approved tags
+            st.markdown(f"🏷️ **Approved Tags:** `{st.session_state.approved_tags[file_name]}`")
+     
 
     output_df.append({
         "index": index,
@@ -118,14 +131,11 @@ for index, row in input_df.iterrows():
 output_df = pd.DataFrame(output_df)
 
 st.write("Approved tags")
-st.dataframe(output_df)
+st.dataframe(output_df[["file", "approved_tags"]], use_container_width = True, hide_index = True)
 
 st.download_button("Download data as csv",
                    data = output_df.to_csv(index = False),
                    file_name = "approved_tags_iamges.csv",
                    mime = "text/csv")
 
-# save_to_mongodb = st.button("Save to MongoDB")
-# if save_to_mongodb:
-#     update_dataframe_to_mongodb(dataframe = output_df, collection = IMG_COLLECTION)
 
